9b6d24caffedef1242664aac3e752dbef555f39b
Alexis-Elaxis <52383750+Alexis-Elaxis@users.noreply.github.com>
2022-02-11 22:03:12 +0100

    Encore et toujours...

--- index.js
+++ index.js
@@ -19,11 +19,11 @@ var requestSettings = {
 request(requestSettings, function (error, response, body) {
     if (!error && response.statusCode == 200) {
         var feed = GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(body);
-        console.log(feed);
         feed.entity.forEach(function(entity) {
         if (entity.trip_update) {
             console.log(entity.trip_update);
         }
+        console.log(entity.tripUpdate)
       });
     }
     //console.log(response.statusCode)
