ed4873d43069bf0729e7377485654312b594555b
Alexis-Elaxis <52383750+Alexis-Elaxis@users.noreply.github.com>
2022-02-11 18:57:43 +0100

    Ajout de la récupération de données

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 require('dotenv').config()
 const {TwitterClient} = require('twitter-api-client')
+const GtfsRealtimeBindings = require('gtfs-realtime-bindings');
+const request = require('request');
 
 const TwitterApp = new TwitterClient({
     apiKey: process.env.TWITTER_API_KEY,
@@ -8,3 +10,18 @@ const TwitterApp = new TwitterClient({
     accessTokenSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 })
 
+var requestSettings = {
+    method: 'GET',
+    url: 'https://data.centrevaldeloire.fr/api/v2/catalog/datasets/agglobus-offre-theorique-mobilite-reseau-urbain-de-bourges/files/07b1b166df5e5f866ad75589b3824978',
+    encoding: null
+  };
+  request(requestSettings, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var feed = GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(body);
+      feed.entity.forEach(function(entity) {
+        if (entity.trip_update) {
+          console.log(entity.trip_update);
+        }
+      });
+    }
+  });
